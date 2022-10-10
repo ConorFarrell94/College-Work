97cb8702c65188fe2dd657c11904cef848e528c9
Conor Farrell <conorfarrell187@gmail.com>
2022-10-10 19:39:11 +0100

    commit

---
+++ Year 4/Rich Web Applications/Lab 01/problem3/Content.js
@@ -0,0 +1,38 @@
+//array of images
+let catsImages = [
+    "https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png",
+    "https://e3.365dm.com/21/03/768x432/skynews-cats-missing-microchip_5315182.jpg?20210323142004",
+    "https://www.thetimes.co.uk/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2Fc3836660-7846-11eb-80c3-8cc375faed89.jpg?crop=5729%2C3222%2C187%2C805&resize=1200",
+    "https://i0.wp.com/post.healthline.com/wp-content/uploads/2020/07/petting_pet_cat-1296x728-header.jpg?w=1155&h=1528",
+	"https://lh3.googleusercontent.com/pw/AL9nZEXYJlrVkYoKIkpx07_3F4HOiTiOheaoaiRAcwrUg3C613-jkzEubJ3k8Z9fDjG5IfVqCzorphZ00vp6mIyB79GtCsoyV69xXe9cqrA0zglgrcvYhH2UP4cDR88WTm1AmuyCxQHAWCB5JzKD7eD94dtNZA=w690-h920-no"
+];
+
+//reverse through array of images
+//getting random image from the array we created before (we use math.floor and math.random to grab a random index in the array)
+const imgs = document.getElementsByTagName("img");
+for(let i = 0; i < imgs.length; i++) {
+    const randomImg = Math.floor(Math.random() * catsImages.length)
+    imgs[i].src = catsImages[randomImg]
+}
+//do the same for h1 elements
+const headers = document.getElementsByTagName("h1");
+for (let i = 0; i < headers.length; i++){
+    headers[i].innerText = "Cats are awesome.";
+}
+//do the same for p elements
+const p = document.getElementsByTagName("p");
+for (let i = 0; i < p.length; i++){
+    p[i].innerText = "This website is now about cats.";
+}
+
+// attempt at dark mode
+// TODO: Some images seem to be inverted, gotta figure out how to stop this, doesn't happen to all of them
+document.querySelector("html").style.filter = "invert(1)"
+let media = document.querySelectorAll("img, picture, video, background-image");
+media.forEach((mediaItem) => {
+    mediaItem.style.filter = "invert(1)"
+})
+const myStyle = document.createElement("style");
+myStyle.innerText = 'body { font-family: Comic Sans MS; }';
+document.head.appendChild(myStyle);
+
