6802fc0087d9b0aa207014784592079a10d185c0
Conor Farrell <conorfarrell187@gmail.com>
2023-02-16 18:12:50 +0000

    fix

--- Year 4/Semester 2/Enterprise App Dev/lab1/assets/js/bs-init.js
+++ Year 4/Semester 2/Enterprise App Dev/lab1/assets/js/bs-init.js
@@ -1,16 +1,21 @@
-
 if (window.innerWidth < 768) {
-	[].slice.call(document.querySelectorAll('[data-bss-disabled-mobile]')).forEach(function (elem) {
-		elem.classList.remove('animated');
-		elem.removeAttribute('data-bss-hover-animate');
-		elem.removeAttribute('data-aos');
-		elem.removeAttribute('data-bss-parallax-bg');
-		elem.removeAttribute('data-bss-scroll-zoom');
-	});
+	[].slice
+		.call(document.querySelectorAll("[data-bss-disabled-mobile]"))
+		.forEach(function (elem) {
+			elem.classList.remove("animated");
+			elem.removeAttribute("data-bss-hover-animate");
+			elem.removeAttribute("data-aos");
+			elem.removeAttribute("data-bss-parallax-bg");
+			elem.removeAttribute("data-bss-scroll-zoom");
+		});
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-	if ('AOS' in window) {
-		AOS.init();
-	}
-}, false);
+document.addEventListener(
+	"DOMContentLoaded",
+	function () {
+		if ("AOS" in window) {
+			AOS.init();
+		}
+	},
+	false
+);
--- Year 4/Semester 2/Enterprise App Dev/lab1/assets/js/scripts.js
+++ Year 4/Semester 2/Enterprise App Dev/lab1/assets/js/scripts.js
@@ -1,4 +1,4 @@
-$('#myList a').on('click', function (e) {
-    e.preventDefault()
-    $(this).tab('show')
-})
+$("#myList a").on("click", function (e) {
+	e.preventDefault();
+	$(this).tab("show");
+});
